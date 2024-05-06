3dcd7d0031e8345337ff92d707ebe0ef0b8c04b9
Andrea Jao <ndrjao@gmail.com>
2024-05-06 14:02:54 -0700

    Completed app.py and tests

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models import db, dbx, Cupcake
 
 app = Flask(__name__)
@@ -17,14 +17,50 @@ app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 
 @app.get("/api/cupcakes")
 def show_all_cupcakes():
-    """Show all cupcakes in database"""
-    
+    """Show all cupcakes in database"""  # FIXME: give example of the return json in the docstring
+
     q_cupcakes = db.select(Cupcake)
+    # FIXME: good habit to always order when querying for multiple things otherwise it's
     cupcakes = dbx(q_cupcakes).scalars().all()
     serialized = [c.serialize() for c in cupcakes]
-    
+
     return jsonify(cupcakes=serialized)
+
 
 @app.get("/api/cupcakes/<int:cupcake_id>")
 def show_single_cupcake(cupcake_id):
-    
+    """Show a single cupcake given its id."""  # FIXME: give example of the return json in the docstring
+
+    cupcake = db.get_or_404(Cupcake, cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+
+@app.post("/api/cupcakes")
+def create_single_cupcake():
+    """Create cupcake from posted JSON data and return it.
+    Returns JSON {'cupcake':{id, flavor, size, rating, image_url}}
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json["image_url"] or None
+    # Right now, if someone is passing in an "", the default won't apply
+    # our default will apply if the response evaluates to falsy, so we can set it to None so the DB will use it vs ""
+
+    new_cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image_url=image_url
+    )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
+    # option 2: we could return everything in a list, whether single or multiple elements to keep the data structure the same
--- models.py
+++ models.py
@@ -5,45 +5,47 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 dbx = db.session.execute
 
+
 class Cupcake(db.Model):
     """Cupcake model"""
-    
+
     __tablename__ = "cupcakes"
-    
+
     id = db.mapped_column(
         db.Integer,
         db.Identity(),
         primary_key=True
     )
-    
+
     flavor = db.mapped_column(
         db.String(50),
         nullable=False
     )
-    
+
     size = db.mapped_column(
         db.String(15),
         nullable=False
     )
-    
+
     rating = db.mapped_column(
         db.Integer,
         nullable=False
     )
-    
+
     image_url = db.mapped_column(
         db.String(500),
+        # FIXME: we want to make this a global var (for urls) for reusaibility and imports
         default="https://tinyurl.com/demo-cupcake",
         nullable=False
     )
-    
+
     def serialize(self):
         """serialize to dictionary"""
-        
+
         return {
             "id": self.id,
             "flavor": self.flavor,
             "size": self.size,
             "rating": self.rating,
-            "img_url": self.image_url
+            "image_url": self.image_url
         }
