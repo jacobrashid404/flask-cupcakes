8501a78c69f2970148d6b967cad3900b5e2e898f
Jacob <jacobrashid404@gmail.com>
2024-05-06 12:20:19 -0700

    Cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,49 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+dbx = db.session.execute
+
+class Cupcake(db.Model):
+    """Cupcake model"""
+    
+    __tablename__ = "cupcakes"
+    
+    id = db.mapped_column(
+        db.Integer,
+        db.Identity(),
+        primary_key=True
+    )
+    
+    flavor = db.mapped_column(
+        db.String(50),
+        nullable=False
+    )
+    
+    size = db.mapped_column(
+        db.String(15),
+        nullable=False
+    )
+    
+    rating = db.mapped_column(
+        db.Integer,
+        nullable=False
+    )
+    
+    image_url = db.mapped_column(
+        db.String(500),
+        default="https://tinyurl.com/demo-cupcake",
+        nullable=False
+    )
+    
+    def serialize(self):
+        """serialize to dictionary"""
+        
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "img_url": self.image_url
+        }
